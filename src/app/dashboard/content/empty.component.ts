1ec382d4f77817fc3cc05c2d8fdbf5a3bc2fb929
Yogesh Vernekar <yogeshvernekar99@gmail.com>
2023-01-06 15:35:55 +0530

    Jan6th

--- src/app/dashboard/content/empty.component.ts
+++ src/app/dashboard/content/empty.component.ts
@@ -3,12 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'empty-content',
   template: `
-    <img
-      class="center"
-      src="../../../assets/img/notfound.png"
-      width="400"
-      height="600"
-    />
+    <div class="notfound">
+      <img
+        class="center"
+        src="../../../assets/img/notfound.png"
+        width="400"
+        height="400"
+      />
+    </div>
   `,
   styleUrls: ['./content.component.css'],
 })
